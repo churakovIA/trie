5007983fc239aa7b541e7318e1f883bf13366760
churakovIA <40851219+churakovIA@users.noreply.github.com>
2019-05-28 13:55:39 +0300

    populate DB

--- src/test/java/ru/churakov/trie/service/AbstractServiceTest.java
+++ src/test/java/ru/churakov/trie/service/AbstractServiceTest.java
@@ -3,8 +3,6 @@ package ru.churakov.trie.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
@@ -13,13 +11,10 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
         "classpath:spring/spring-app.xml",
         "classpath:spring/spring-db.xml"
 })
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@Sql(scripts = "classpath:db/populateDB-test.sql", config = @SqlConfig(encoding = "UTF-8"))
 abstract class AbstractServiceTest {
 
     protected final Logger log = LoggerFactory.getLogger(getClass());
-
-    @Autowired
-    private Environment env;
 
     static {
         // needed only for java.util.logging (postgres driver)
